2119688cab295cce9cc6cae5d8681bcea2268385
jerck1 <johncafre@gmail.com>
2020-02-12 10:52:02 -0500

    pushing

--- JohnErickCabreraRamirez-ejercicio07.py
+++ JohnErickCabreraRamirez-ejercicio07.py
@@ -33,10 +33,71 @@ X = data[:,:4]
 sigma=0.1
 
 
-# In[12]:
+# In[36]:
 
 
 w=2*np.ones(len(X))/sigma**2
+
+
+# In[44]:
+
+
+beta=np.ones(len(X[0]+1))
+c=beta[0]
+i
+
+
+# In[48]:
+
+
+def loglikelihood(x, y, beta,sigma):
+    y_model = y_test(x, beta)
+    sum=1/(np.power(2*np.pi,0.5)*sigma)
+    for i in range(len(x)):
+        sum+=(y_model[i]-y)**2
+    return sum
+
+
+# In[40]:
+
+
+def metropolis(x_obs, y_obs, N = len(X)):
+    l_param = [np.array([1/8000.0, 10.0])]
+    sigma_param = np.array([1E-4, 0.5])
+    n_param = len(sigma_param)
+    logposterior = [0]
+    for i in range(1,N):
+        propuesta  = l_param[i-1] + np.random.normal(size=n_param)*sigma_param
+        #print(propuesta)
+        logposterior_viejo = loglikelihood(x_obs, y_obs, l_param[i-1]) + logprior(l_param[i-1])
+        logposterior_nuevo = loglikelihood(x_obs, y_obs, propuesta) + logprior(propuesta)
+
+        r = min(1,np.exp(logposterior_nuevo-logposterior_viejo))
+        alpha = np.random.random()
+        if(alpha<r):
+            l_param.append(propuesta)
+            logposterior.append(logposterior_nuevo)
+        else:
+            l_param.append(l_param[i-1])
+            logposterior.append(logposterior_viejo)
+    
+    l_param = np.array(l_param)
+    l_param = l_param[N//10:,:] # descartamos el primer 10% de la cadena
+    logposterior = np.array(logposterior)
+    logposterior = logposterior[N//10:]
+    return l_param, logposterior
+
+
+# In[41]:
+
+
+metropolis(X[0],Y)
+
+
+# In[38]:
+
+
+
 
 
 # In[34]:
